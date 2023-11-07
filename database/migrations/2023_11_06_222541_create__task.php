eb9e18ef85aa9706fe64929ff821545f146ec576
wojcup <wojcup@gmail.com>
2023-11-07 00:03:07 +0000

    Task Route, Controller and Model
    Preparing the basic steps for the Task controller and the Model. Retrieving the first portion of the data from the database.

---
+++ database/migrations/2023_11_06_222541_create__task.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->bigIncrements('task_id')->unsigned();
+            $table->string('name');
+            $table->longText('description');
+            $table->integer('owner_id');
+            $table->timestamp('created_at')->default( NULL );
+            $table->timestamp('updated_at')->nullable()->default( NULL );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource( '/tasks', TaskController::class )->middleware(['auth']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
