3bacc6325d61336d391c568397d2d32d114ba0ca
wojcup <wojcup@gmail.com>
2023-11-07 00:49:12 +0000

    Display Tasks - improvements
    Adding pagination, corrections to links, applying the excerpt helper. for longer text, conditional message when no tasks are available.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,15 +7,22 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <a href="{{ route('tasks.create') }}" class="btn-link btn-lg mb-2">+ New Task</a>
+
+
             @forelse( $tasks as $task )
                 <div class="my-10 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <h2 class="font-bold text-2xl">{{ $task->name }}</h2>
-                    <p class="mt-2">{{ $task->description }}</p>
+                    <p class="mt-2">{{ Str::limit( $task->description, 200 ) }}</p>
                     <span class="block mt-4 text-sm opacity-70">{{ $task->updated_at ? $task->updated_at->diffForHumans() : $task->created_at?->diffForHumans() }}</span>
                 </div>
             @empty
             <p>There are no tasks for you yet</p>
             @endforelse
+
+
+            {{ $tasks->links() }}
         </div>
     </div>
 </x-app-layout>
